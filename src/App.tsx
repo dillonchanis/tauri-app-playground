b82a9e5e7b0cfd92dec8b9c3d57c7d5a34351e17
dillonchanis <dillonchanis92@gmail.com>
2022-08-01 22:16:50 -0400

    Adding cursor style

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App: Component = () => {
   return (
     <main class="max-w-5xl mx-auto p-6">
       <button
-        class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+        class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:cursor-not-allowed"
         type="button"
         onClick={handleClick}
         disabled={user.loading}
